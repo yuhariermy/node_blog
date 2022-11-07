1597588614cb780f63b30dbceaa62bf5a88b7366
yuhariermy <yuhariermy@gmail.com>
2022-11-07 22:44:52 +0700

    finishing the moderation service

--- comments/index.js
+++ comments/index.js
@@ -1,29 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
 
-const { randomBytes } = require("crypto");
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
 
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/posts/:id/comments", async (req, res) => {
-  const commentId = randomBytes(4).toString("hex"); // make random id number
-  const { content } = req.body; // return all input from the content
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
-  const comments =
-    commentsByPostId[req.params.id] /** it should undefined **/ || []; // the empty array should return if undifined
+  const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content: content, status: "pending" });
+  comments.push({ id: commentId, content, status: "pending" });
+
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
@@ -36,21 +34,24 @@ app.post("/posts/:id/comments", async (req, res) => {
     },
   });
 
-  res.status(200).send(comments);
+  res.status(201).send(comments);
 });
 
 app.post("/events", async (req, res) => {
-  console.log("Recieved events", req.body.type);
+  console.log("Event Received:", req.body.type);
 
   const { type, data } = req.body;
 
   if (type === "CommentModerated") {
-    const { id, postId, status } = data;
+    const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
-    const comment = comments.find((comment) => comment.id === id);
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
     comment.status = status;
 
-    await axios.post("http://localhost:4005", {
+    await axios.post("http://localhost:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
